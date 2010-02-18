4e884f6fdbf0412ce043e210e8ff97ac610cdc19
Romain François <romain@r-enthusiasts.com>
2010-02-18 12:19:14 +0000

    move the XPtr example to Rcpp, since it is used in the unit tests

--- src/RcppExample.cpp
+++ src/RcppExample.cpp
@@ -462,17 +462,3 @@ RcppExport SEXP RcppVectorExample(SEXP vector) {
     return rl;
 }
 
-RcppExport SEXP RcppXPtrExample_create_external_pointer(){
-	std::vector<int> *v = new std::vector<int> ;
-	v->push_back( 1 ) ;
-	v->push_back( 2 ) ;
-	Rcpp::XPtr< std::vector<int> > p(v) ;
-	return p ;
-}
-
-RcppExport SEXP RcppXPtrExample_get_external_pointer(SEXP x){
-	Rcpp::XPtr< std::vector<int> > p(x) ;
-	return Rf_ScalarInteger( p->back( ) ) ;
-}
-
-
