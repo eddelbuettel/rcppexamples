10770b6c963d42d8039fcbeeced30dd08695b4cd
Dirk Eddelbuettel <edd@debian.org>
2017-01-02 09:02:23 -0600

    refining #3 a little, new minor release

--- R/RcppDataFrame.R
+++ R/RcppDataFrame.R
@@ -1,6 +1,6 @@
 ## RcppDataFrame.R: DataFrame example
 ##
-## Copyright (C) 2011 - 2016  Dirk Eddelbuettel and Romain Francois
+## Copyright (C) 2011 - 2017  Dirk Eddelbuettel and Romain Francois
 ##
 ## This file is part of RcppExamples.
 ##
@@ -32,7 +32,7 @@ RcppDataFrame <- function() {
     val <- DataFrameExample(D)
     
     cat("\nAfter call, original and new data frames:\n")
-    # print(val)
+    print(val)
 
-    val
+    invisible(val)
 }
